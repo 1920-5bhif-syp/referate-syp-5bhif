55a8441337fa26dd51f575bf5725b4f5af9a6861
Maximilian Wahl <mexx.wale@gmail.com>
2020-02-07 12:34:28 +0100

    began hand-in of presentation

---
+++ Wahl_SelfhostingChatbots/examples/site/index.js
@@ -0,0 +1,40 @@
+var botui = new BotUI( 'hello-world' );
+var input = document.getElementById( "input" );
+
+input.addEventListener( "keyup", function ( event ) {
+    if ( event.keyCode === 13 ) {
+        event.preventDefault();
+        document.getElementById( "sendButton" ).click();
+    }
+} );
+
+async function sendmessage() {
+    var message = document.getElementById( "input" ).value;
+    botui.message.human( {
+        content: message
+    } );
+    let resp = await fetch( 'http://localhost:5005/webhooks/rest/webhook', {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify( {
+            message: message
+        } )
+    } ).then( response => { return response.json() } );
+    console.log( resp );
+    console.log( botui )
+    for ( var i = 0; i < resp.length; i++ ) {
+        if ( resp[ i ].text ) {
+            botui.message.bot( {
+                content: resp[ i ].text
+            } );
+        }
+        else if ( resp[ i ].image ) {
+            botui.message.bot( {
+                content: "![cute image](" + resp[ i ].image + ")"
+            } );
+        }
+
+    }
+    document.getElementById( "input" ).value = "";
+}
